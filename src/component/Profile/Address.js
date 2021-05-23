8627ca826fba663566d878038a88b00ad45d7539
Rihab Chouikh <chouikh.rihab@gmail.com>
2021-05-23 13:37:55 +0200

    first commit

---
+++ src/component/Profile/Address.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Address() {
+    return (
+        <div className="adresse">
+            <h2>-- El Alia Bizerte 7016 --</h2>
+        </div>
+    )
+}
+
+export default Address
+
+
+
---
+++ src/component/Profile/ProfilePhoto.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProfilePhoto() {
+    return (
+        <div className="image">
+            <img clasName="shadow" src="/fotoProfile.jpg" alt="profilePhoto" width="450" height="450" />
+        </div>
+    )
+}
+
+export default ProfilePhoto
